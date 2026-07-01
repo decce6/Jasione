f6f22703b788584ab051c976ef93c1fee43978c9
decce6 <decce6@proton.me>
2026-07-01 20:18:07 +0800

    Use ReflectionUtil for defining class

--- src/service/java/me/decce/transformingbase/service/fabric/FabricPreLaunchEntrypoint.java
+++ src/service/java/me/decce/transformingbase/service/fabric/FabricPreLaunchEntrypoint.java
@@ -3,8 +3,8 @@ package me.decce.transformingbase.service.fabric;
 //? if fabric {
 import me.decce.transformingbase.core.Jasione;
 import me.decce.transformingbase.service.transform.CommonTransformer;
+import me.decce.transformingbase.service.transform.ReflectionUtil;
 import net.fabricmc.loader.api.entrypoint.PreLaunchEntrypoint;
-import net.lenni0451.reflect.ClassLoaders;
 import org.objectweb.asm.tree.ClassNode;
 import org.spongepowered.asm.mixin.MixinEnvironment;
 
@@ -35,7 +35,7 @@ public class FabricPreLaunchEntrypoint implements PreLaunchEntrypoint {
         String className = "org.spongepowered.asm.mixin.transformer.JasioneMixinCoprocessor";
         try (var res = this.getClass().getResourceAsStream("/" + className.replace('.', '/') + ".class")) {
             var bytes = Objects.requireNonNull(res).readAllBytes();
-            var clazz = ClassLoaders.defineClass(classLoader, className, bytes);
+            var clazz = ReflectionUtil.defineClass(classLoader, className, bytes);
             return clazz.getConstructor(Predicate.class).newInstance(predicate);
         }
     }
--- src/service/java/me/decce/transformingbase/service/transform/ReflectionUtil.java
+++ src/service/java/me/decce/transformingbase/service/transform/ReflectionUtil.java
@@ -3,8 +3,8 @@ package me.decce.transformingbase.service.transform;
 import net.lenni0451.reflect.ClassLoaders;
 
 public class ReflectionUtil {
-    public static void defineClass(ClassLoader classLoader, String name, byte[] bytes) {
-        ClassLoaders.defineClass(classLoader, name, bytes);
+    public static Class<?> defineClass(ClassLoader classLoader, String name, byte[] bytes) {
+        return ClassLoaders.defineClass(classLoader, name, bytes);
     }
 
     public static boolean classExists(ClassLoader classLoader, String name) {
